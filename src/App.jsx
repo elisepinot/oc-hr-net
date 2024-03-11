812d17a1ca8f8361f132bd106ef7c6abfad9cecf
elisepinot <elise.pinot@live.fr>
2024-03-11 12:26:04 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+// import { Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className='App'>
+      <p>Test</p>
+    </div>
+  );
+}
+
+export default App;
